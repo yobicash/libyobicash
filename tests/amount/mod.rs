e0e1ab589a5e44ee9ba0b98dc99347cb76f20550
Christian Nyumbayire <chritchens@gmail.com>
2017-07-02 18:28:45 +0200

    Add amount tests

---
+++ tests/amount/mod.rs
@@ -0,0 +1,48 @@
+use libyobicash::amount::*;
+use libyobicash::errors::*;
+use libyobicash::crypto::utils::randombytes;
+
+#[test]
+fn add_amounts_succ() {
+    let a = YAmount::new(1);
+    let b = YAmount::new(2);
+    let c = YAmount::new(3);
+    let c1 = a + b;
+    assert_eq!(c, c1)
+}
+
+#[test]
+fn sub_amounts_succ() {
+    let a = YAmount::new(3);
+    let b = YAmount::new(2);
+    let c = YAmount::new(1);
+    let c1 = a - b;
+    assert_eq!(c, c1)
+}
+
+#[test]
+fn mul_amounts_succ() {
+    let a = YAmount::new(1);
+    let b = YAmount::new(2);
+    let c = YAmount::new(2);
+    let c1 = a * b;
+    assert_eq!(c, c1)
+}
+
+#[test]
+fn div_amounts_succ() {
+    let a = YAmount::new(3);
+    let b = YAmount::new(2);
+    let c = YAmount::new(1);
+    let c1 = a / b;
+    assert_eq!(c, c1)
+}
+
+#[test]
+fn rem_amounts_succ() {
+    let a = YAmount::new(3);
+    let b = YAmount::new(2);
+    let c = YAmount::new(1);
+    let c1 = a % b;
+    assert_eq!(c, c1)
+}
