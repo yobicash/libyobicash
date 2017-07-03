f72ef695eb3e7e184a574f6f630ea65d852b68ea
Christian Nyumbayire <chritchens@gmail.com>
2017-07-03 18:46:56 +0200

    Output API cleanup

--- src/models/output/mod.rs
+++ src/models/output/mod.rs
@@ -38,35 +38,29 @@ impl Output {
     }
 
     pub fn get_to(&self) -> Address {
-        unreachable!()
+        self.to.to_owned()
     }
 
     pub fn set_to(&mut self, to: &Address) -> Result<Self> {
-        unreachable!()
+        check_address(to)?;
+        self.to = to.to_owned();
+        Ok(self.to_owned())
     }
 
     pub fn get_amount(&self) -> Amount {
-        unreachable!()
-    }
-
-    pub fn set_amount(&mut self, amount: &Amount) -> Result<Self> {
-        unreachable!()
+        self.amount.to_owned()
     }
 
     pub fn get_size(&self) -> u32 {
-        unreachable!()
+        self.size
     }
 
-    pub fn get_checksum(&self) -> u32 {
-        unreachable!()
+    pub fn get_checksum(&self) -> Result<Hash> {
+        hash(self.data.to_owned().as_slice())
     }
 
     pub fn get_data(&self) -> Vec<u8> {
-        unreachable!()
-    }
-
-    pub fn set_data(&mut self, data: &Vec<u8>) -> Result<Self> {
-        unreachable!()
+        self.data.to_owned()
     }
 
     pub fn check(&self) -> Result<()> {
@@ -82,7 +76,7 @@ impl Output {
             Amount::new(self.size) != self.amount.to_owned() {
             return Err(ErrorKind::InvalidAmount.into());
         }
-        let checksum = hash(self.data.to_owned().as_slice())?;
+        let checksum = self.get_checksum()?;
         if self.checksum != checksum {
             return Err(ErrorKind::InvalidChecksum.into());
         }
