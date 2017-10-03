d32382482a47c796dc9c909d1880ed2a2260a5d7
chritchens <chritchens@gmail.com>
2017-10-03 20:46:52 +0200

    Add input binary serialization

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -1,9 +1,11 @@
 use curve25519_dalek::edwards::ValidityCheck;
+use byteorder::{ByteOrder, BigEndian, WriteBytesExt};
 use crypto::digest::YDigest;
 use crypto::elliptic::scalar::YScalar;
 use crypto::elliptic::point::YPoint;
 use crypto::zkp::schnorr_protocol::SchnorrProtocolPublic;
 use output::YOutput;
+use std::io::Write;
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug, Default)]
 pub struct YPartialInput {
@@ -25,9 +27,42 @@ impl YPartialInput {
     }
   }
 
-  pub fn to_bytes(&self) -> Vec<u8> { unreachable!() }
+  pub fn to_bytes(&self) -> Option<Vec<u8>> {
+    let mut buf = Vec::new();
+    match buf.write(&self.id.to_bytes()[..]) {
+      Ok(_) => {},
+      Err(_) => { return None; },
+    }
+    match buf.write_u32::<BigEndian>(self.idx) {
+      Ok(_) => {},
+      Err(_) => { return None; },
+    }
+    match buf.write_u64::<BigEndian>(self.height) {
+      Ok(_) => {},
+      Err(_) => { return None; },
+    }
+    Some(buf)
+  }
 
-  pub fn from_bytes(b: &[u8]) -> Option<YPartialInput> { unreachable!() }
+  pub fn from_bytes(b: &[u8]) -> Option<YPartialInput> {
+    if b.len() != 76 {
+      return None;
+    }
+
+    let mut pi = YPartialInput::default();
+
+    if let Some(_id) = YDigest::from_bytes(&b[0..64]) {
+      pi.id = _id;
+    } else {
+      return None;
+    }
+
+    pi.idx = BigEndian::read_u32(&b[64..68]);
+
+    pi.height = BigEndian::read_u64(&b[68..76]);
+
+    Some(pi)
+  }
 
   pub fn complete(self, g: YPoint, t: YPoint, c: YScalar, r: YScalar) -> Option<YInput> {
     YInput::new(self.id, self.idx, self.height, g, t, c, r)
@@ -78,13 +113,82 @@ impl YInput {
     i.complete(g, t, c, r)
   }
 
-  pub fn from_output(out: &YOutput) -> YInput {
-    unreachable!()
+  pub fn to_bytes(&self) -> Option<Vec<u8>> {
+    let mut buf = Vec::new();
+    match buf.write(&self.id.to_bytes()[..]) {
+      Ok(_) => {},
+      Err(_) => { return None; },
+    }
+    match buf.write_u32::<BigEndian>(self.idx) {
+      Ok(_) => {},
+      Err(_) => { return None; },
+    }
+    match buf.write_u64::<BigEndian>(self.height) {
+      Ok(_) => {},
+      Err(_) => { return None; },
+    }
+    match buf.write(&self.g.to_bytes()[..]) {
+      Ok(_) => {},
+      Err(_) => { return None; },
+    }
+    match buf.write(&self.t.to_bytes()[..]) {
+      Ok(_) => {},
+      Err(_) => { return None; },
+    }
+    match buf.write(&self.c.to_bytes()[..]) {
+      Ok(_) => {},
+      Err(_) => { return None; },
+    }
+    match buf.write(&self.r.to_bytes()[..]) {
+      Ok(_) => {},
+      Err(_) => { return None; },
+    }
+    Some(buf)
   }
 
-  pub fn to_bytes(&self) -> Vec<u8> { unreachable!() }
+  pub fn from_bytes(b: &[u8]) -> Option<YInput> {
+    if b.len() != 204 {
+      return None;
+    }
 
-  pub fn from_bytes(b: &[u8]) -> Option<YInput> { unreachable!() }
+    let mut input = YInput::default();
+
+    if let Some(_id) = YDigest::from_bytes(&b[0..64]) {
+      input.id = _id;
+    } else {
+      return None;
+    }
+
+    input.idx = BigEndian::read_u32(&b[64..68]);
+
+    input.height = BigEndian::read_u64(&b[68..76]);
+
+    if let Some(_g) = YPoint::from_bytes(&b[76..108]) {
+      input.g = _g; 
+    } else {
+      return None;
+    }
+
+    if let Some(_t) = YPoint::from_bytes(&b[108..140]) {
+      input.t = _t; 
+    } else {
+      return None;
+    }
+
+    if let Some(_c) = YScalar::from_bytes(&b[140..172]) {
+      input.c = _c; 
+    } else {
+      return None;
+    }
+
+    if let Some(_r) = YScalar::from_bytes(&b[172..204]) {
+      input.r = _r; 
+    } else {
+      return None;
+    }
+
+    Some(input)
+  }
 
   pub fn verify(&self, out: &YOutput) -> bool {
     let prot = SchnorrProtocolPublic {
